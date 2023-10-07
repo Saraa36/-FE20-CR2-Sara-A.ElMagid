511161443a6211c9ba184f7d42c4f57366c08c63
Sara A.ElMagid <saramagid27@gmail.com>
2023-10-08 00:52:39 +0300

    CR -2

---
+++ data.js
@@ -0,0 +1,65 @@
+ let weekdays = `[
+    {
+       "day": "Monday",
+        "taskName" : "Zoom meeting at 9 am",
+        "description" :"Team meeting",
+        "img" : "imgs/work2.jpg",
+        "importance":0
+
+    },{ 
+        "day": "Tuesday",
+        "taskName" : "Pick my mother from the airport",
+        "description" :"Airport at 4 pm",
+        "img" : "imgs/sun.jpg",
+        "importance":0
+    
+        
+    },{
+        "day": "Wednesday",
+        "taskName" : "Go shopping",
+        "description" :"Weekly groceries",
+        "img" : "imgs/shopping-.jpg",
+        "importance":0
+    },
+
+    {   "day": "Thursday",
+        "taskName" : "Meet my friends",
+        "description" :"Fun time",
+        "img" : "imgs/teddy.jpg",
+        "importance":0
+
+    },
+    {
+        "day": "Friday",
+        "taskName" : "Take the dog for a walk",
+        "description" :"Pet day",
+        "img" : "imgs/pet.jpg",
+        "importance":0
+
+    },
+    {
+        "day": "Saturday",
+        "taskName" : "Go to gym",
+        "description" :"Gym day",
+        "img" : "imgs/yoga.jpg",
+        "importance":0
+    },{
+        "day": "Sunday",
+        "taskName" : "Meals for the week",
+        "description" :"Cook day",
+        "img" : "imgs/cook.jpg",
+        "importance":0
+    },{
+        "day": "Monday",
+        "taskName" : "Work",
+        "description" :"Home office",
+        "img" : "imgs/work2.jpg",
+        "importance":0
+    },{
+        "day": "Tuesday",
+        "taskName" : "Work",
+        "description" :"On site",
+        "img" : "imgs/work.jpg",
+        "importance":0
+    }
+]`;
---
+++ script.js
@@ -0,0 +1,63 @@
+let newArr = JSON.parse(weekdays); // convert string to an object
+
+console.log(newArr);
+newArr.forEach (item => {
+    document.getElementById('result').innerHTML += `
+    <div class="card" style="width: 20rem;">
+  <img src="${item.img}" class="card-img-top" alt="${item.fname}">
+  <div class="card-body">
+    <h5 class="card-title">Today's Task</h5>
+    <p class="card-text">${item.day} ${'-'} ${item.description}</p>
+    <p class="card-text">${item.taskName}</p>
+    <p><button class="btn btn-primary myNo">Importance <span class = 'like'>${item.importance}</span></button></p>
+
+    <button class="btn btn-primary myBtn">Done</button>
+    <button class="btn btn-primary myBtn">Reschedule</button>
+
+
+  </div>
+</div>
+    `;
+})
+    
+let btns = document.querySelectorAll(".myBtn");
+
+btns.forEach(btn =>{ 
+    btn.addEventListener("mouseup",() => {
+        btn.style.backgroundColor = 'green'
+    })
+
+})
+let btn1 = document.querySelectorAll('.myNo');
+
+btn1.forEach(imp =>{
+    imp.addEventListener ("mousedown",() =>{
+        imp.style.backgroundColor = 'red'
+    })
+
+})
+btn1.forEach ((imp, i) => {
+    imp.addEventListener('click', function(){
+        incLikes(i);
+    })
+    
+} );
+
+function incLikes (i){
+    newArr[i].importance++;
+    document.querySelectorAll('.like')[i].innerText = newArr[i].importance
+    
+}
+document.getElementById('btn').addEventListener ('click', function()
+    { let newVar = newArr.sort((a, b) => b.likes - a.likes);
+
+        console.log(newVar);
+
+
+    }
+)
+
+
+
+
+     
